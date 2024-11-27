abe60988d5d3d29b17c03fc732a9ca6bc7b3a3cb
HunterWarters <jv421834@gmail.com>
2024-11-28 00:43:04 +0800

    2nd year  project
    project for

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 const questions = [
   {
-              question: "1. question palceholder",
-              options: ["A.", "B.","C.", "D."],
+              question: "1.What is 1+1?",
+              options: ["A.5", "B.4","C.3", "D.2"],
               answer: "D."
   },
   {
-              question: "2. question placeholder",
-              options: ["A.", "B.", "C.", "D."],
+              question: "2.What is 10/10?",
+              options: ["A.1", "B.13", "C.5", "D.100 B.C"],
               answer: "A."
   },
   {
-               question: "3. question placeholder",
-               options: ["A.", "B.", "C.", "D."],
+               question: "3.What is 2+2?",
+               options: ["A.3", "B.4", "C.7", "D.43"],
                answer: "B."
   },
   {
-               question: "4. question placeholder",
-               options: ["A.","B.","C.","D."],
+               question: "4.what is the dog sound",
+               options: ["A.GRRRRR","B.AWOOO","C.ARF","D.AW"],
                answer: "D."
   },
   {
-               question: "5. question placeholder",
-               options: ["A.","B.", "C.","D."],
+               question: "5.Ano gagawin sa I.T",
+               options: ["A.Mag shift","B.Mag batak mag code", "C.Matulog","D.Magsipag"],
                answer: "A."
   },
   {
-               question: "6. question placeholder",
-               options: ["A.", "B.","C.","D."],
+               question: "6.Ilan ang butas ng rebisco",
+               options: ["A.33", "B.56","C.33","D.55"],
                answer: "B."
   },
   {
-               question: "7. question placeholder",
+               question: "7.question placeholder",
                options: ["A.", "B.", "C.", "D."],
                answer: "D."
   },
   {
-               question: "8. question placeholder",
+               question: "8.question placeholder",
                options: ["A.", "B.", "C.", "D."],
                answer: "D."
   },
   {
-               question: "9. question placeholder",
+               question: "9.question placeholder",
                options: [ "A.","B","C","D"],
                answer: "A."
   },
@@ -59,7 +59,7 @@ const questions = [
   const submitButton = document.getElementById("submit");
   const resultElement = document.getElementById("result");
   const scoreElement = document.getElementById("score");
-// RENDERS THE CURRENT QUESTIONS AND ANSWERS ON SCREEN 
+// RENDERS THE CURRENT QUESTIONS,ANSWERS ON SCREEN 
 //AND ITERATE THROUGH THE QUESTIONS AND ANSWERS THAT IS STORED IN AN ARRAY OF OBJECTS
   function loadQuestion() {
           const currentQuestion = questions[currentQuestionIndex];
